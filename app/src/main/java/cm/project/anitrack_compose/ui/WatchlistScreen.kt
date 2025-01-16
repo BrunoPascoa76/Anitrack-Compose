fc8bdb5ea8732283568d38aa1c0e8e68c568e15a
BrunoPascoa76 <brunopascoa03@ua.pt>
2025-01-16 17:13:35 +0000

    backdoors functionality for watchlist mostly done

--- app/src/main/java/cm/project/anitrack_compose/ui/WatchlistScreen.kt
+++ app/src/main/java/cm/project/anitrack_compose/ui/WatchlistScreen.kt
@@ -1,6 +1,8 @@
 package cm.project.anitrack_compose.ui
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
@@ -10,6 +12,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -46,12 +49,14 @@ fun WatchlistScreen(
         },
         bottomBar = { BottomNavBar(navController) }
     ) { innerPadding ->
-        val possibleStatuses =
-            listOf(MediaListStatus.CURRENT, MediaListStatus.PLANNING, MediaListStatus.COMPLETED)
-        when (selectedIndex) {
-            0 -> WatchListScreen(possibleStatuses[0])
-            1 -> WatchListScreen(possibleStatuses[1])
-            2 -> WatchListScreen(possibleStatuses[2])
+        Box(modifier = Modifier.padding(innerPadding)) {
+            val possibleStatuses =
+                listOf(MediaListStatus.CURRENT, MediaListStatus.PLANNING, MediaListStatus.COMPLETED)
+            when (selectedIndex) {
+                0 -> WatchListScreen(possibleStatuses[0])
+                1 -> WatchListScreen(possibleStatuses[1])
+                2 -> WatchListScreen(possibleStatuses[2])
+            }
         }
     }
 }
