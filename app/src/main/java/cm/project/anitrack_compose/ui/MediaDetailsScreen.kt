f01b2f20f0ac134811c38c96c698e4cb85366940
BrunoPascoa76 <brunopascoa03@ua.pt>
2025-01-18 11:07:25 +0000

    basic info

--- app/src/main/java/cm/project/anitrack_compose/ui/MediaDetailsScreen.kt
+++ app/src/main/java/cm/project/anitrack_compose/ui/MediaDetailsScreen.kt
@@ -4,14 +4,20 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
@@ -26,8 +32,11 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import cm.project.anitrack_compose.graphql.GetMediaDetailsQuery
 import cm.project.anitrack_compose.ui.components.LoadingScreen
 import cm.project.anitrack_compose.viewModels.MediaDetailsViewModel
 import coil.compose.AsyncImage
@@ -54,7 +63,7 @@ fun MediaDetailsScreen(mediaId: Int, navController: NavController) {
                     BannerComponent(navController, media!!.bannerImage)
                 }
                 item {
-                    BasicInfoComponent()
+                    BasicInfoComponent(media!!)
                 }
                 stickyHeader {
                     val tabs = listOf(
@@ -80,7 +89,7 @@ fun MediaDetailsScreen(mediaId: Int, navController: NavController) {
                 }
                 item {
                     when (selectedTab) {
-                        0 -> ExtendedInfoComponent()
+                        0 -> ExtendedInfoComponent(media!!)
                         1 -> RelationsComponent()
                         2 -> CharactersComponent()
                         3 -> StaffComponent()
@@ -108,6 +117,7 @@ private fun BannerComponent(navController: NavController, imageUrl: String?) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(bottom = 5.dp)
     ) {
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current).data(imageUrl).build(),
@@ -134,12 +144,51 @@ private fun BannerComponent(navController: NavController, imageUrl: String?) {
 }
 
 @Composable
-private fun BasicInfoComponent() {
+private fun BasicInfoComponent(media: GetMediaDetailsQuery.Media) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp, vertical = 5.dp)
+    ) {
+        Row {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current).data(media.coverImage?.large)
+                    .build(),
+                contentDescription = null,
+                contentScale = ContentScale.FillHeight,
+                modifier = Modifier.height(150.dp)
+            )
+
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 10.dp, vertical = 5.dp)
+            ) {
+                Text(
+                    media.title?.english ?: media.title?.native ?: "",
+                    fontWeight = FontWeight.Bold
+                )
+                HorizontalDivider(thickness = 3.dp, color = MaterialTheme.colorScheme.primary)
+                if (media.title?.native != null) {
+                    Text("Native Title:")
+                    Text(media.title.native)
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Text(
+                        "Status: " + media.status.toString().lowercase()
+                            .replaceFirstChar { it.uppercase() }.replace("_", " "),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+        }
+    }
 }
 
 
 @Composable
-private fun ExtendedInfoComponent() {
+private fun ExtendedInfoComponent(media: GetMediaDetailsQuery.Media) {
+    ElevatedCard(modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp)) {
+
+    }
 }
 
 @Composable
