68f6b7316796b70e2b01ac5a98eecf05c727cf12
BrunoPascoa76 <brunopascoa03@ua.pt>
2025-01-21 15:45:14 +0000

    fixed discover

--- app/src/main/java/cm/project/anitrack_compose/viewModels/DiscoverViewModel.kt
+++ app/src/main/java/cm/project/anitrack_compose/viewModels/DiscoverViewModel.kt
@@ -4,12 +4,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import cm.project.anitrack_compose.getSeason
+import cm.project.anitrack_compose.graphql.DiscoverMediaPageQuery
 import cm.project.anitrack_compose.graphql.type.MediaSeason
 import cm.project.anitrack_compose.graphql.type.MediaSort
 import cm.project.anitrack_compose.paging.DiscoverMediaPage
 import cm.project.anitrack_compose.repositories.GraphQLRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
@@ -20,21 +24,34 @@ class DiscoverViewModel @Inject constructor(private val graphQLRepository: Graph
     private val _selectedIndex = MutableStateFlow(0)
     val selectedIndex = _selectedIndex.asStateFlow()
 
-    var pager = Pager(PagingConfig(pageSize = 20)) {
-        DiscoverMediaPage(
-            graphQLRepository,
-            listOf(MediaSort.POPULARITY_DESC),
-        )
-    }.flow.cachedIn(viewModelScope)
+    private val pagers = List(4) { index ->
+        createPager(getParamsForTab(index))
+    }
 
-    fun updatePager(sortCriteria: List<MediaSort>, season: MediaSeason? = null, year: Int? = null) {
-        pager = Pager(PagingConfig(pageSize = 20)) {
-            DiscoverMediaPage(
-                graphQLRepository,
-                sortCriteria,
-                season,
-                year
-            )
+    fun getPagerForCurrentTab() = pagers[selectedIndex.value]
+
+    private fun getParamsForTab(index: Int): Triple<List<MediaSort>, MediaSeason?, Int?> {
+        return when (index) {
+            0 -> Triple(listOf(MediaSort.TRENDING_DESC), null, null)
+            1 -> Triple(listOf(MediaSort.POPULARITY_DESC), null, null)
+            2 -> {
+                val (currentSeason, currentYear) = getSeason(0)
+                Triple(listOf(MediaSort.POPULARITY_DESC), currentSeason, currentYear)
+            }
+
+            3 -> {
+                val (nextSeason, nextYear) = getSeason(1)
+                Triple(listOf(MediaSort.POPULARITY_DESC), nextSeason, nextYear)
+            }
+
+            else -> Triple(listOf(MediaSort.POPULARITY_DESC), null, null)
+        }
+    }
+
+    private fun createPager(params: Triple<List<MediaSort>, MediaSeason?, Int?>): Flow<PagingData<DiscoverMediaPageQuery.Medium>> {
+        val (sortCriteria, season, year) = params
+        return Pager(PagingConfig(pageSize = 20)) {
+            DiscoverMediaPage(graphQLRepository, sortCriteria, season, year)
         }.flow.cachedIn(viewModelScope)
     }
 
